e931270131ed3e83d63dbfc1d7f264e68c4b16d9
Trung Giang <trunggiang.official@gmail.com>
2026-01-03 17:04:31 +0000

    ## Error Type Recoverable Error
    ## Error Message
    Hydration failed becau

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,6 +1,6 @@
 import type { Task } from './types';
 
-const now = new Date();
+const now = new Date('2024-01-01T00:00:00');
 
 const getRelativeDate = (dayOffset: number, hour: number = 0, minute: number = 0) => {
     const date = new Date(now);
