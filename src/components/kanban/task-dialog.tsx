e4f918dddc831189d8072f1a7f1fe35196de7f64
Trung Giang <trunggiang.official@gmail.com>
2026-01-04 05:54:31 +0000

    cho phần công việc nhích lên gần với thanh nhiệm vụ/công việc

--- src/components/kanban/task-dialog.tsx
+++ src/components/kanban/task-dialog.tsx
@@ -478,7 +478,7 @@ export function TaskDialog({ isOpen, onOpenChange, onSubmit, taskToEdit }: TaskD
                   </DialogFooter>
               </TabsContent>
 
-              <TabsContent value="subtasks" className="flex-1 flex flex-col min-h-0 space-y-4 pt-4">
+              <TabsContent value="subtasks" className="flex-1 flex flex-col min-h-0 space-y-4">
                 <div className="flex-1 overflow-y-auto pr-6 -mr-6 py-2 custom-scrollbar space-y-4">
                   <div className="space-y-2">
                       <Accordion type="multiple" className="w-full space-y-2">
