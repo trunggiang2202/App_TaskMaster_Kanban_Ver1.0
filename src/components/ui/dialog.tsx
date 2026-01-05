12632e35d68ca2d8c6b9ebc775e4a17fd9ed2aa8
Trung Giang <trunggiang.official@gmail.com>
2026-01-05 03:33:29 +0000

    muốn nút X khi hover có nền màu đỏ

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -45,7 +45,7 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none data-[state=open]:text-muted-foreground">
+      <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none disabled:pointer-events-none data-[state=open]:text-muted-foreground hover:bg-destructive hover:text-destructive-foreground">
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
