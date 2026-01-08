8aaa8596b2ad7ada3d0fecaf3d3b5f1cb6cbbfeb
Trung Giang <trunggiang.official@gmail.com>
2026-01-08 03:10:41 +0000

    cho nền thanh phía sau trắng đục

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -13,7 +13,7 @@ const Progress = React.forwardRef<
   <ProgressPrimitive.Root
     ref={ref}
     className={cn(
-      "relative h-4 w-full overflow-hidden rounded-full bg-sidebar-border",
+      "relative h-4 w-full overflow-hidden rounded-full bg-background/20",
       className
     )}
     {...props}
